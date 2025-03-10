3cff9c92252606e32d1a8c70118e58137937ff00
santosfabin <fsfabianosantos03@gmail.com>
2025-03-10 19:36:01 -0300

    fix: fixing .env, config/index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 
 const config = {
-	NODE_ENV: process.env.NODE_ENV || "development",
-	PORT: process.env.PORT || 3000,
-	SECRET_KEY: process.env.SECRET_KEY || "chave_aleatoria_qualquer"
+  NODE_ENV: process.env.NODE_ENV, // Sem fallback
+  PORT: process.env.PORT || 3000,
+  SECRET_KEY: process.env.SECRET_KEY // Agora depende apenas do arquivo .env
 };
+
 
 module.exports = config;
